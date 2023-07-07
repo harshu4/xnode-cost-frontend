883088fdb06e0b9836b54421f93427068c22c532
Bruno Laureano <brunolsantos152@gmail.com>
2023-07-06 23:26:09 -0300

    fix: changing ui

--- components/Hero3/index.tsx
+++ components/Hero3/index.tsx
@@ -12,14 +12,16 @@ const Hero3 = () => {
           alt="logo"
           width={100}
           height={20}
-          className="md:w-1/10 mx-auto mb-2 w-1/5 transition-all duration-200 hover:z-20 hover:scale-110"
+          className="mx-auto mb-2 w-1/5 transition-all duration-200 hover:z-20 hover:scale-110 md:w-1/12"
         />
         <p className="mx-auto mb-12 text-xl font-bold leading-relaxed  md:mb-2 md:text-xl xl:text-4xl">
-          Information asymmetry, a result of this control, has given rise to
-          social inequality, scandals, polarization, and corruption. In extreme
-          cases, it has even triggered wars. With such a massive concentration
-          of data and information, surveillance capitalism has begun to
-          influence politics and culture profoundly
+          <span className="text-[#6B6B6B]">
+            Information asymmetry, a result of this control, has given rise to
+            social inequality, scandals, polarization, and corruption. In
+            extreme cases, it has even triggered wars. With such a
+          </span>
+          massive concentration of data and information, surveillance capitalism
+          has begun to influence politics and culture profoundly
         </p>
         <Image
           src="/images/lines/Line3.svg"
