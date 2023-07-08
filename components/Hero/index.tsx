77bcb723fd249052a6244449e4146855f361d194
Bruno Santos <brunolsantos152@gmail.com>
2023-07-07 22:55:42 -0300

    fix: image size

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
                   alt="logo"
                   width={100}
                   height={20}
-                  className="mx-auto h-[402px] w-[747px] scale-110 transition-all duration-200 hover:z-20 md:mt-1 lg:mt-20"
+                  className="mx-auto h-[402px] w-[707px] scale-110 transition-all duration-200 hover:z-20 md:mt-1 lg:mt-20"
                 />
                 {/* <h1 className="mb-5 text-3xl font-bold leading-tight sm:text-4xl sm:leading-tight md:text-7xl md:leading-tight">
                   Join the ecosystem
