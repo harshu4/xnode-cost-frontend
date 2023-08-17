ddaa759ea4df5daee525cf7c3e9deaf1011edbd4
Bruno Santos <brunolsantos152@gmail.com>
2023-08-17 19:46:35 -0300

    fix: removing old logo

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>L3A Ecosystem</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="L3A Ecosystem" />
-      <link rel="icon" href="/images/logo/l3a.ico" />
+      <link rel="icon" href="/images/l3a.ico" />
     </>
   )
 }
