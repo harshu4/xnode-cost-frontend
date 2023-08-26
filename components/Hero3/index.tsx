c5c276f7a40bf199d4bc6119d843d9606b5acd30
Bruno Santos <brunolsantos152@gmail.com>
2023-08-26 09:52:43 -0300

    feat: starting

--- components/Hero3/index.tsx
+++ components/Hero3/index.tsx
@@ -1,26 +1,82 @@
 const Hero3 = () => {
   return (
     <>
-        <div className="h-10 pt-[111px]">
-
-</div>
+      <div className="pt-[111px]"></div>
       <section
         id="home"
         className="relative z-10 overflow-hidden bg-white px-[20px] text-[#000000] lg:pt-[195px]"
       >
         <div className="mx-auto">
-          <img
-            src="/images/hero3/man.svg"
-            alt="image"
-            className={`absolute top-0 left-0 right-0 z-[-1] m-auto w-[720px]`}
-          />
-          <div className="flex justify-center text-[64px] font-bold tracking-[2.2%]">
-            Becomin<span className="text-white">g an OpenMesh Expert</span>
-            Network
+          <div className="mx-auto">
+            <img
+              src="/images/hero3/man.svg"
+              alt="image"
+              className={`absolute top-0 left-0 right-0 z-[-1] m-auto w-[720px]`}
+            />
+            <div className="flex justify-center text-[64px] font-bold tracking-[2.2%]">
+              Becomin<span className="text-white">g an OpenMesh Expert</span>
+              Network
+            </div>
+          </div>
+          <div className="mx-auto  mt-[301px] grid w-[1250px] grid-cols-3 items-center justify-center gap-x-[120px] gap-y-[146px]">
+            <div className="w-[294px]">
+              <div className="text-[24px] font-bold !leading-[150%] tracking-[2.2%]">
+                Cost, speed, and scale
+              </div>
+              <div className="mt-[15px] text-[20px] font-medium text-[#959595]">
+                Deploy, and manage data infrastructure and data applications in
+                minutes instead of months
+              </div>
+            </div>
+            <div className="w-[294px]">
+              <div className="text-[24px] font-bold !leading-[150%] tracking-[2.2%]">
+                Cost, speed, and scale
+              </div>
+              <div className="mt-[15px] text-[20px] font-medium text-[#959595]">
+                Deploy, and manage data infrastructure and data applications in
+                minutes instead of months
+              </div>
+            </div>
+            <div className="w-[294px]">
+              <div className="text-[24px] font-bold !leading-[150%] tracking-[2.2%]">
+                Cost, speed, and scale
+              </div>
+              <div className="mt-[15px] text-[20px] font-medium text-[#959595]">
+                Deploy, and manage data infrastructure and data applications in
+                minutes instead of months
+              </div>
+            </div>
+            <div className="w-[294px]">
+              <div className="text-[24px] font-bold !leading-[150%] tracking-[2.2%]">
+                Cost, speed, and scale
+              </div>
+              <div className="mt-[15px] text-[20px] font-medium text-[#959595]">
+                Deploy, and manage data infrastructure and data applications in
+                minutes instead of months
+              </div>
+            </div>
+            <div className="w-[294px]">
+              <div className="text-[24px] font-bold !leading-[150%] tracking-[2.2%]">
+                Cost, speed, and scale
+              </div>
+              <div className="mt-[15px] text-[20px] font-medium text-[#959595]">
+                Deploy, and manage data infrastructure and data applications in
+                minutes instead of months
+              </div>
+            </div>
+            <div className="w-[294px]">
+              <div className="text-[24px] font-bold !leading-[150%] tracking-[2.2%]">
+                Cost, speed, and scale
+              </div>
+              <div className="mt-[15px] text-[20px] font-medium text-[#959595]">
+                Deploy, and manage data infrastructure and data applications in
+                minutes instead of months
+              </div>
+            </div>
           </div>
         </div>
+        <div className="mx-auto mt-[89px] flex h-[1px] w-[900px] bg-[#D4D4D4]"></div>
       </section>
-      <div className="mt-[89px] h-[1px] bg-[#D4D4D4]"></div>
     </>
   )
 }
