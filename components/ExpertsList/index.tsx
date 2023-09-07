45eb549a69c7d796902f34ac4ed606bed010bc4b
Bruno Laureano <brunolsantos152@gmail.com>
2023-09-06 23:40:40 -0300

    feat: new ui and cms

--- components/ExpertsList/index.tsx
+++ components/ExpertsList/index.tsx
@@ -23,21 +23,38 @@ const ExpertsList = () => {
   async function getData() {
     const data = await getPosts()
     setTestimonial(data as any)
-    console.log('testimonial setado')
     console.log(data)
     console.log(data[0].logo)
   }
 
-  //   async function getData2() {
-  //     const data = await fetch('http://localhost:3333/api/posts')
-  //     console.log('fetche')
-  //     console.log(data)
-  //   }
+  async function getData2() {
+    const response = await fetch(
+      'https://cdn.contentful.com/spaces/y6pyp0ob27zb/environments/master/entries?access_token=xqv_WzuGSzMvF_MeZ5Hm_iICfM7e-zwGC1EeW8TfKSI',
+      {
+        method: 'GET',
+        next: {
+          revalidate: 300000000000,
+        },
+      },
+    )
+
+    if (response.ok) {
+      const data = await response.json() // Extrai o corpo da resposta como um objeto JavaScript
+      console.log('Fetched')
+      console.log(`Limit: ${data.limit}`) // Loga o campo "limit"
+      const final = data.items.map((item) => item.fields)
+      console.log(final)
+    } else {
+      console.log(
+        `Failed to fetch data: ${response.status} ${response.statusText}`,
+      )
+    }
+  }
 
   useEffect(() => {
     console.log('useEffect chamado')
     getData()
-    // getData2()
+    getData2()
   }, [])
   return (
     <section className="bg-white pl-[90px] pr-[64px] pt-[97px] pb-[131px] text-[#000]">
