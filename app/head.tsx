46f1048569570a4c42bd8013a63e8fde47255e87
Bruno Laureano <brunolsantos152@gmail.com>
2023-09-07 23:36:01 -0300

    feat: changing ui

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>OpenMesh Community</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="L3A Ecosystem" />
+      <meta name="description" content="OpenMesh Community" />
       {/* <link rel="icon" href="/images/l3a.ico" /> */}
     </>
   )
