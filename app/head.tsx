cb8f41e5549aeda3373433fe07a02ec92fbf5131
Bruno Santos <brunolsantos152@gmail.com>
2023-09-22 11:27:55 -0300

    fix: fixing openmesh desc

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Openmesh Community</title>
+      <title>Openmesh Data marketplace</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Openmesh Community" />
+      <meta name="description" content="Openmesh Data marketplace" />
       <link rel="icon" href="/openmesh-ico-logo.png" />
     </>
   )
