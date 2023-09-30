964d651da52f06c172bd63bddcd85297947191e1
Bruno Laureano <brunolsantos152@gmail.com>
2023-09-29 22:27:14 -0300

    feat: adding footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
-const Hero = () => {
+const Footer = () => {
+  const useCasesOptions = [
+    'Financial Analysis',
+    'Blockchain Transactions',
+    'DEXs and CEXs',
+    'Gas Optimization',
+    'Crypto Liquidity',
+  ]
   return (
     <>
       <section
@@ -19,14 +26,26 @@ const Hero = () => {
               Openmesh, 2023
             </div>
           </div>
-          <div className="flex gap-x-[15px] text-[9px] font-medium text-[#959595] md:gap-x-[18px] md:text-[10px] lg:gap-x-[21px] lg:text-[11px] lg:!leading-[150%] xl:gap-x-[24px] xl:text-[13px] 2xl:gap-x-[30px] 2xl:text-[16px]">
-            <div className="max-w-[360px]">
-              <span className="text-[#000]">Scalable</span> <br /> Xnode adapts
-              to the growing needs of a project, making it a reliable solution
-              throughout a project's lifecycle
+          <div className="flex gap-x-[20px] text-[9px] font-normal text-[#959595] md:gap-x-[24px] md:text-[10px] lg:gap-x-[28px] lg:text-[11px] lg:!leading-[150%] xl:gap-x-[32px] xl:text-[13px] 2xl:gap-x-[40px] 2xl:text-[16px]">
+            <div className="border-t-[1px] border-[#D9D9D9] pb-[8px] pt-[4px] lg:pt-[12px] lg:pb-[12px] 2xl:pt-[15px] 2xl:pb-[15px] ">
+              <div className="pr-[20px] md:pr-[24px] lg:pr-[28px] xl:pr-[32px] 2xl:pr-[40px]">
+                <div className="pb-[8px]  font-bold text-[#000] lg:pb-[12px] lg:leading-[19px] 2xl:pb-[15px]">
+                  Use Cases
+                </div>
+                <div className="lg:leading-[220%]">
+                  {useCasesOptions.map((useCase, index) => (
+                    <div
+                      key={index}
+                      className={`cursor-pointer text-[#000] hover:text-[#757575]`}
+                    >
+                      {useCase}
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
-            <div className="max-w-[240px] text-[8px]  md:max-w-[220px] md:text-[10px] lg:mt-[78px] lg:text-[12px]  xl:text-[13px] 2xl:text-[16px]">
-              <div className="mt-[8px] border-b-[1px] border-t-[1px] border-[#D9D9D9] pb-[8px]  lg:pt-[12px] lg:pb-[12px]  2xl:pt-[15px] 2xl:pb-[15px] ">
+            <div className="max-w-[240px] text-[8px]  md:max-w-[220px] md:text-[10px] lg:text-[12px]  xl:text-[13px] 2xl:text-[16px]">
+              <div className="border-b-[1px] border-t-[1px] border-[#D9D9D9] pb-[8px]  pt-[4px] text-[#000] lg:pt-[12px] lg:pb-[12px]  2xl:pt-[15px] 2xl:pb-[15px] ">
                 <div className="pb-[8px] font-bold lg:pb-[12px] lg:leading-[19px] 2xl:pb-[15px]">
                   Suggest a new feature
                 </div>
@@ -37,8 +56,8 @@ const Hero = () => {
                 </div>
               </div>
               <div className="mt-[8px] pb-[8px] lg:mt-[12px] lg:pb-[12px] 2xl:mt-[15px] 2xl:pb-[15px]">
-                <div className="pb-[8px] font-bold lg:pb-[12px] lg:leading-[19px] 2xl:pb-[15px]">
-                  Encounter any issue?
+                <div className="pb-[8px] font-bold  text-[#000] lg:pb-[12px] lg:leading-[19px] 2xl:pb-[15px]">
+                  Have more questions?
                 </div>
                 <div className=" lg:!leading-[150%]">
                   {' '}
@@ -56,4 +75,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Footer
--- components/Presets/index.tsx
+++ components/Presets/index.tsx
@@ -93,7 +93,7 @@ const Presets = () => {
         <div className="mt-[12.5px] grid justify-between gap-x-[8px] gap-y-[15px] md:mt-[15px] md:gap-x-[9px] md:gap-y-[18px] lg:mt-[17.5px] lg:gap-x-[10.5px] lg:gap-y-[21px] xl:gap-x-[12px] xl:gap-y-[24px] 2xl:mt-[25px] 2xl:grid-cols-3 2xl:gap-x-[15px] 2xl:gap-y-[30px]">
           {preSetsOptions.map((option, index) => (
             <div
-              className="h-[400px] rounded-[5px] border-[0.5px] border-[#D9D9D9] p-[10px] md:p-[12px] lg:p-[14px] xl:p-[16px] 2xl:p-[20px]"
+              className="h-[400px] transform cursor-pointer rounded-[5px] border-[0.5px] border-[#D9D9D9] p-[10px] transition-transform hover:scale-105 md:p-[12px] lg:p-[14px] xl:p-[16px] 2xl:p-[20px]"
               key={index}
             >
               <div className="relative h-full w-full max-w-[300px]">
