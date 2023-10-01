679ebe12d5fe75e6baa79741fb4042acb0db6835
Bruno Laureano <brunolsantos152@gmail.com>
2023-10-01 17:33:28 -0300

    feat: removing unused

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 /* eslint-disable no-unused-vars */
 'use client'
 
-import L3AContributors from '@/components/About/L3AContributors'
 import ScrollUp from '@/components/Common/ScrollUp'
 import Hero from '@/components/Hero'
-import SuccessStories from '@/components/SuccessStories'
-import ExpertsList from '@/components/DataProvidersList'
 import { Inter } from '@next/font/google'
 import { useRef } from 'react'
-import { RevealWrapper } from 'next-reveal'
 import FirstStep from '@/components/FirstStep'
 import Footer from '@/components/Footer'
 
