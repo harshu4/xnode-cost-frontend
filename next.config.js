a0685b0f6d22074975fba63f7105a056e2662954
Bruno Santos <brunolsantos152@gmail.com>
2023-10-26 21:52:15 -0300

    fix: changing sideba

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  assetPrefix: 'https://openmesh-expert-community.vercel.app',
+  // assetPrefix: 'https://openmesh-expert-community.vercel.app',
 }
 
 module.exports = nextConfig
