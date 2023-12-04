85c7b7a1551954b0c58b8c367b10b80c80edbe92
Bruno Laureano <brunolsantos152@gmail.com>
2023-12-04 11:18:06 -0300

    feat: new deployment tool

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,14 @@ export default function Head() {
       <title>Openmesh Xnode</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Openmesh Xnode" />
-      <link rel="icon" href="/openmesh-ico-logo.png" />
+      <link
+        rel="icon"
+        href={`${
+          process.env.NEXT_PUBLIC_ENVIRONMENT === 'PROD'
+            ? process.env.NEXT_PUBLIC_BASE_PATH
+            : ''
+        }/openmesh-blue.png`}
+      />
     </>
-  );
+  )
 }
