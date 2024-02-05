66760ffb39320d51fd245a72892cc7d55446de57
Bruno Laureano <brunolsantos152@gmail.com>
2024-02-05 18:40:47 -0300

    feat: new selection function

--- components/PythiaLandingPage/DisplayCost.tsx
+++ components/PythiaLandingPage/DisplayCost.tsx
@@ -14,15 +14,16 @@ type provider = {
 }
 
 type subSelectionOption = {
-    name: string
-    desc: string
-  }  
+  name: string
+  desc: string
+}
 
 interface ModalI {
   providers: provider[]
-  subSelectionOption: 
+  subSelectionOption: subSelectionOption
 }
-const CostLandingPage = () => {
+
+const DisplayCost = ({ providers, subSelectionOption }: ModalI) => {
   const [newMessageHtml, setNewMessageHtml] = useState('')
   const [nextStep, setNextStep] = useState<boolean>(false)
   const [selectionOptionSelected, setSelectionOptionSelected] = useState({
@@ -114,112 +115,42 @@ const CostLandingPage = () => {
 
   return (
     <>
-      <div className="flex h-full px-[50px]  pb-[40px] text-[16px]  font-normal  text-[#000] lg:text-[20px]">
-        <div className="flex h-full w-full rounded-xl bg-[#F9F9F9] px-[45px] pt-[75px] shadow-md">
-          <div>
-            <div className="text-[18px] lg:text-[26px]">
-              Selection function & use case
-            </div>
-            <div className="mt-[54px] flex gap-x-[50px]">
-              {selectionOptions.map((option, index) => (
-                <div
-                  onClick={() => {
-                    setSelectionOptionSelected(option)
-                  }}
-                  key={index}
-                  className={`h-fit cursor-pointer text-[14px] font-bold text-[#AEAEAE] hover:text-[#000] lg:text-[20px] ${
-                    selectionOptionSelected?.name === option.name &&
-                    '!text-[#000000]'
-                  }`}
-                >
-                  {option.name}
-                </div>
-              ))}
-            </div>
-            <div className="mt-[66px] flex gap-x-[100px]">
-              <div className="grid gap-y-[43px] ">
-                {subSelectionOptions.map((subOption, index) => (
-                  <div
-                    onClick={() => {
-                      setSubSelectionOptionSelected(subOption)
-                    }}
-                    key={index}
-                    className={`h-fit max-w-[590px] cursor-pointer rounded-[10px] border-[1px] border-[#A4A4A4] py-[18px] px-[20px] text-[14px] font-bold text-[#000]  hover:bg-[#e9e9e949] lg:text-[20px]`}
-                  >
-                    <div className="flex justify-between gap-x-[110px]">
-                      <div>
-                        {' '}
-                        <div>{subOption.name}</div>
-                        <div className="text-[12px] font-normal lg:text-[16px]">
-                          {subOption.desc}
-                        </div>
-                      </div>
-                      <div
-                        className={`my-auto h-[22px] w-[22px] rounded-full border-[1px] border-[#939191] ${
-                          subSelectionOptionSelected?.name === subOption.name &&
-                          '!border-[#0354EC] bg-[#0354EC]'
-                        }`}
-                      ></div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-              <div className="flex min-h-[100%] w-[1px] bg-[#000]"></div>
-              <div className="text-[18px] font-bold text-[#AEAEAE] lg:text-[26px]">
-                <div>Pick providers to compare</div>
-                <div className="mt-[60px] grid gap-y-[43px]">
-                  {providersSelectionOptions.map((providerOption, index) => (
-                    <div
-                      onClick={() => {
-                        const providers = [...providerSelectionOptionSelected]
-                        const exists = providers.findIndex(
-                          (provider) => provider.name === providerOption.name,
-                        )
-                        if (exists === -1) {
-                          setProviderSelectionOptionSelected([
-                            providerOption,
-                            ...providerSelectionOptionSelected,
-                          ])
-                        } else {
-                          providers.splice(exists, 1)
-                          setProviderSelectionOptionSelected(providers)
-                        }
-                      }}
-                      key={index}
-                      className={`text-[14px] font-bold text-[#AEAEAE] lg:text-[20px]`}
-                    >
-                      <div className="flex items-center gap-x-[12px]">
-                        <div
-                          className={`my-auto h-[47px]   w-[47px] cursor-pointer rounded-[5px]  border-[1px] border-[#939191] hover:border-[#0354EC] hover:bg-[#023ca7] hover:bg-[#0354EC] ${
-                            findItemProvider(providerOption.name) &&
-                            '!border-[#0354EC] bg-[#0354EC]'
-                          }`}
-                        ></div>
-                        <div>
-                          {' '}
-                          <div>{providerOption.name}</div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className="mt-[80px] ml-[30px]">
-              <div
-                onClick={() => {
-                  setNextStep(true)
-                }}
-                className="w-fit cursor-pointer rounded-[5px] bg-[#0354EC] px-[60px] py-[12px] text-[14px] font-bold text-[#FFFFFF] hover:bg-[#023ca7] lg:text-[20px]"
-              >
-                Compare
+      <div className="">
+        <div
+          className={`h-fit max-w-[590px] cursor-pointer rounded-[10px] border-[1px] border-[#A4A4A4] py-[18px] px-[20px] text-[14px] font-bold text-[#000]  hover:bg-[#e9e9e949] lg:text-[20px]`}
+        >
+          <div className="flex justify-between gap-x-[110px]">
+            <div>
+              {' '}
+              <div>{subSelectionOption.name}</div>
+              <div className="text-[12px] font-normal lg:text-[16px]">
+                {subSelectionOption.desc}
               </div>
             </div>
           </div>
+        </div>
+        <div className="mt-[56px] flex gap-x-[100px]">
+          {providers.map((provider, index) => (
+            <div
+              key={index}
+              className={`flex justify-between text-[14px] font-bold text-[#AEAEAE] lg:text-[20px]`}
+            >
+              <div className="flex items-center gap-x-[12px]">
+                <div>
+                  {' '}
+                  <div>{provider.name}</div>
+                </div>
+              </div>
+
+              {index !== providers.length - 1 && (
+                <div className=" ml-[90px] flex min-h-[100%] w-[1px] bg-[#000]"></div>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </>
   )
 }
 
-export default CostLandingPage
+export default DisplayCost
--- components/PythiaLandingPage/index.tsx
+++ components/PythiaLandingPage/index.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 import 'react-quill/dist/quill.snow.css' // import styles
 import './react-quill.css'
+import DisplayCost from './DisplayCost'
 
 const QuillNoSSRWrapper = dynamic(import('react-quill'), {
   ssr: false,
@@ -116,6 +117,7 @@ const CostLandingPage = () => {
                 <div
                   onClick={() => {
                     setSelectionOptionSelected(option)
+                    setNextStep(false)
                   }}
                   key={index}
                   className={`h-fit cursor-pointer text-[14px] font-bold text-[#AEAEAE] hover:text-[#000] lg:text-[20px] ${
@@ -127,85 +129,102 @@ const CostLandingPage = () => {
                 </div>
               ))}
             </div>
-            <div className="mt-[66px] flex gap-x-[100px]">
-              <div className="grid gap-y-[43px] ">
-                {subSelectionOptions.map((subOption, index) => (
-                  <div
-                    onClick={() => {
-                      setSubSelectionOptionSelected(subOption)
-                    }}
-                    key={index}
-                    className={`h-fit max-w-[590px] cursor-pointer rounded-[10px] border-[1px] border-[#A4A4A4] py-[18px] px-[20px] text-[14px] font-bold text-[#000]  hover:bg-[#e9e9e949] lg:text-[20px]`}
-                  >
-                    <div className="flex justify-between gap-x-[110px]">
-                      <div>
-                        {' '}
-                        <div>{subOption.name}</div>
-                        <div className="text-[12px] font-normal lg:text-[16px]">
-                          {subOption.desc}
+            {nextStep ? (
+              <div className="mt-[66px]">
+                <DisplayCost
+                  providers={providerSelectionOptionSelected}
+                  subSelectionOption={subSelectionOptionSelected}
+                />
+              </div>
+            ) : (
+              <div>
+                <div className="mt-[66px] flex gap-x-[100px]">
+                  <div className="grid gap-y-[43px] ">
+                    {subSelectionOptions.map((subOption, index) => (
+                      <div
+                        onClick={() => {
+                          setSubSelectionOptionSelected(subOption)
+                        }}
+                        key={index}
+                        className={`h-fit max-w-[590px] cursor-pointer rounded-[10px] border-[1px] border-[#A4A4A4] py-[18px] px-[20px] text-[14px] font-bold text-[#000]  hover:bg-[#e9e9e949] lg:text-[20px]`}
+                      >
+                        <div className="flex justify-between gap-x-[110px]">
+                          <div>
+                            {' '}
+                            <div>{subOption.name}</div>
+                            <div className="text-[12px] font-normal lg:text-[16px]">
+                              {subOption.desc}
+                            </div>
+                          </div>
+                          <div
+                            className={`my-auto h-[22px] w-[22px] rounded-full border-[1px] border-[#939191] ${
+                              subSelectionOptionSelected?.name ===
+                                subOption.name &&
+                              '!border-[#0354EC] bg-[#0354EC]'
+                            }`}
+                          ></div>
                         </div>
                       </div>
-                      <div
-                        className={`my-auto h-[22px] w-[22px] rounded-full border-[1px] border-[#939191] ${
-                          subSelectionOptionSelected?.name === subOption.name &&
-                          '!border-[#0354EC] bg-[#0354EC]'
-                        }`}
-                      ></div>
+                    ))}
+                  </div>
+                  <div className="flex min-h-[100%] w-[1px] bg-[#000]"></div>
+                  <div className="text-[18px] font-bold text-[#AEAEAE] lg:text-[26px]">
+                    <div>Pick providers to compare</div>
+                    <div className="mt-[60px] grid gap-y-[43px]">
+                      {providersSelectionOptions.map(
+                        (providerOption, index) => (
+                          <div
+                            onClick={() => {
+                              const providers = [
+                                ...providerSelectionOptionSelected,
+                              ]
+                              const exists = providers.findIndex(
+                                (provider) =>
+                                  provider.name === providerOption.name,
+                              )
+                              if (exists === -1) {
+                                setProviderSelectionOptionSelected([
+                                  providerOption,
+                                  ...providerSelectionOptionSelected,
+                                ])
+                              } else {
+                                providers.splice(exists, 1)
+                                setProviderSelectionOptionSelected(providers)
+                              }
+                            }}
+                            key={index}
+                            className={`text-[14px] font-bold text-[#AEAEAE] lg:text-[20px]`}
+                          >
+                            <div className="flex items-center gap-x-[12px]">
+                              <div
+                                className={`my-auto h-[47px]   w-[47px] cursor-pointer rounded-[5px]  border-[1px] border-[#939191] hover:border-[#0354EC] hover:bg-[#023ca7] hover:bg-[#0354EC] ${
+                                  findItemProvider(providerOption.name) &&
+                                  '!border-[#0354EC] bg-[#0354EC]'
+                                }`}
+                              ></div>
+                              <div>
+                                {' '}
+                                <div>{providerOption.name}</div>
+                              </div>
+                            </div>
+                          </div>
+                        ),
+                      )}
                     </div>
                   </div>
-                ))}
-              </div>
-              <div className="flex min-h-[100%] w-[1px] bg-[#000]"></div>
-              <div className="text-[18px] font-bold text-[#AEAEAE] lg:text-[26px]">
-                <div>Pick providers to compare</div>
-                <div className="mt-[60px] grid gap-y-[43px]">
-                  {providersSelectionOptions.map((providerOption, index) => (
-                    <div
-                      onClick={() => {
-                        const providers = [...providerSelectionOptionSelected]
-                        const exists = providers.findIndex(
-                          (provider) => provider.name === providerOption.name,
-                        )
-                        if (exists === -1) {
-                          setProviderSelectionOptionSelected([
-                            providerOption,
-                            ...providerSelectionOptionSelected,
-                          ])
-                        } else {
-                          providers.splice(exists, 1)
-                          setProviderSelectionOptionSelected(providers)
-                        }
-                      }}
-                      key={index}
-                      className={`text-[14px] font-bold text-[#AEAEAE] lg:text-[20px]`}
-                    >
-                      <div className="flex items-center gap-x-[12px]">
-                        <div
-                          className={`my-auto h-[47px]   w-[47px] cursor-pointer rounded-[5px]  border-[1px] border-[#939191] hover:border-[#0354EC] hover:bg-[#023ca7] hover:bg-[#0354EC] ${
-                            findItemProvider(providerOption.name) &&
-                            '!border-[#0354EC] bg-[#0354EC]'
-                          }`}
-                        ></div>
-                        <div>
-                          {' '}
-                          <div>{providerOption.name}</div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
+                </div>
+                <div className="mt-[80px] ml-[30px]">
+                  <div
+                    onClick={() => {
+                      setNextStep(true)
+                    }}
+                    className="w-fit cursor-pointer rounded-[5px] bg-[#0354EC] px-[60px] py-[12px] text-[14px] font-bold text-[#FFFFFF] hover:bg-[#023ca7] lg:text-[20px]"
+                  >
+                    Compare
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="mt-[80px] ml-[30px]">
-              <div
-                onClick={() => {
-                  setNextStep(true)
-                }}
-                className="w-fit cursor-pointer rounded-[5px] bg-[#0354EC] px-[60px] py-[12px] text-[14px] font-bold text-[#FFFFFF] hover:bg-[#023ca7] lg:text-[20px]"
-              >
-                Compare
-              </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
