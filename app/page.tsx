8bc5c574de19890cbc40799f911697c2019b3f8f
Bruno Laureano <brunolsantos152@gmail.com>
2024-02-08 22:33:21 -0300

    feat: removing unecessary button

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,6 @@ import ScrollUp from '@/components/Common/ScrollUp'
 import Hero from '@/components/Hero'
 import { Inter } from '@next/font/google'
 import { useRef } from 'react'
-import FirstStep from '@/components/FirstStep'
-import Footer from '@/components/Footer'
-import Console from '@/components/Console'
-import LandingPage from '@/components/LandingPage'
 import PythiaLandingPage from '@/components/PythiaLandingPage'
 
 const inter = Inter({ subsets: ['latin'] })
